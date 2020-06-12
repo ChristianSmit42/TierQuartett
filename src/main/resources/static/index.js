1d2000f4000483eeff0c4082ef7ffb29dc911f9d
Christian Smit <chrsmit@web.de>
2020-06-12 15:49:24 +0200

    Add js create Card And insert Cards into DOM

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,7 +1,56 @@
 
 async function listAnimals(){
     const response = await fetch("http://localhost:8080/animals");
-    const animalList = (await response).json();
-    console.log(animalList);
+    const animalList = await response.json();
+    return animalList;
 }
-listAnimals();
+function renderAnimal(animal){
+    const animalCard = document.createElement("div");
+    animalCard.setAttribute("class", "card");
+    animalCard.setAttribute("style", "width:15rem");
+    animalCard.appendChild(renderCardBody(animal));
+    return animalCard;
+}
+
+function renderCardBody(animal){
+    const animalCardBody = document.createElement("div");
+    animalCardBody.setAttribute("class", "card-body");
+
+    const animalCardTitle = document.createElement("h5");
+    animalCardTitle.setAttribute("class", "card-title");
+    animalCardTitle.innerHTML = animal.name;
+
+    animalCardBody.appendChild(animalCardTitle);
+
+    const animalCardText = document.createElement("div");
+    animalCardText.setAttribute("class", "card-text");
+
+    const animalGender = document.createElement("li");
+    animalGender.innerHTML = "gender: "+animal.gender;
+
+    const animalAge = document.createElement("li");
+    animalAge.innerHTML = "age: " + animal.age;
+
+    const animalCute = document.createElement("li");
+    animalCute.innerHTML = "cute: "+animal.cute;
+
+    animalCardText.appendChild(animalCute);
+    animalCardText.appendChild(animalAge);
+    animalCardText.appendChild(animalGender);
+
+    animalCardBody.appendChild(animalCardText);
+    return animalCardBody;
+}
+
+async function createCardsAnimals(){
+    const animals = await listAnimals();
+    const element = document.getElementById("animals");
+    animals.forEach(animal => {
+        const column = document.createElement("div");
+        column.setAttribute("class", "col-12 col-sm-6 col-md-3 col-sm-2");
+        const animalCard = renderAnimal(animal);
+        column.appendChild(animalCard);
+        element.appendChild(column);
+    });
+}
+createCardsAnimals().catch(console.error);
