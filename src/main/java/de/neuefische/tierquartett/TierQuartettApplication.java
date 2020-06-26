b3881f78c453e5832cac86fe88d45d7dacf324ae
Christian Smit <chrsmit@web.de>
2020-06-26 11:38:54 +0200

    Add Stuff an such

---
+++ src/main/java/de/neuefische/tierquartett/TierQuartettApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.tierquartett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierQuartettApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TierQuartettApplication.class, args);
+    }
+
+}
